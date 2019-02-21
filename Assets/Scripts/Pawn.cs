448f3b494bec879f36abb6924cb9ba05300965a5
JohnyMastro <johnymastro@gmail.com>
2019-02-21 13:21:57 -0500

    Added: -Pawn and Player classes -Pacman Prefab

---
+++ Assets/Scripts/Pawn.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Direction { UP, DOWN, LEFT, RIGHT };
+
+//Base abstract class to be inherited by AI and Playable Characters
+public abstract class Pawn : MonoBehaviour {
+    protected Direction mDirection = Direction.LEFT;
+    protected float mSpeed = 0.05f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Move(Direction direction)
+    {
+        Vector2 directionVector;
+        switch (direction)
+        {
+            case Direction.UP:
+                directionVector = Vector2.up;
+                break;
+            case Direction.DOWN:
+                directionVector = Vector2.down;
+                break;
+            case Direction.LEFT:
+                directionVector = Vector2.left;
+                break;
+            case Direction.RIGHT:
+                directionVector = Vector2.right;
+                break;
+            default:
+                directionVector = Vector2.zero;
+                break;
+        }
+        mDirection = direction;
+        directionVector *= mSpeed;
+        transform.position = new Vector2(transform.position.x + directionVector.x, transform.position.y + directionVector.y);
+    }
+
+    public abstract void Die();
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//this class is to give the player control over pacman
+public class Player : Pawn {
+
+	// Use this for initialization
+	void Start () {
+        mDirection = Direction.LEFT;
+    }
+
+   // Update is called once per frame
+	void Update () {
+        PlayerControl();
+    }
+
+    //used to poll Player's input
+    void PlayerControl()
+    {
+        //right
+        if (Input.GetButton("Horizontal") && Input.GetAxisRaw("Horizontal") > 0)
+        {
+            Move(Direction.RIGHT);
+        }
+        //left
+        else if (Input.GetButton("Horizontal") && Input.GetAxisRaw("Horizontal") < 0)
+        {
+            Move(Direction.LEFT);
+        }
+        //Down
+        else if (Input.GetButton("Vertical") && Input.GetAxisRaw("Vertical") < 0)
+        {
+            Move(Direction.DOWN);
+        }
+        //Up
+        else if (Input.GetButton("Vertical") && Input.GetAxisRaw("Vertical") > 0)
+        {
+            Move(Direction.UP);
+        }
+    }
+
+    public override void Die()
+    {
+
+    }
+}
